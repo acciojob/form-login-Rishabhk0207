05827c90f75e250e41bd10fa020320b1ff7bef02
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-27 23:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+		event.preventDefault();
+		const first = document.querySelector('input[name="fname"]').value.trim();
+		const last = document.querySelector('input[name="lname"]').value.trim();
+
+		if(first && last){
+			alert(`${first} ${last}`);
+		}else{
+			alert('Please fill the name');
+		}
 
 }
